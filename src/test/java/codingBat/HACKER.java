9234be28dd978395fc56b1f554a73c9024649e5a
KaldybekovY <94867959+KaldybekovY@users.noreply.github.com>
2022-11-12 09:36:22 -0600

    adding functionality

--- src/test/java/codingBat/HACKER.java
+++ src/test/java/codingBat/HACKER.java
@@ -4,6 +4,6 @@ import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
 
 public class HACKER {
     public static void main(String[] args) {
-
+        System.out.println("Hello Group4");
     }
 }

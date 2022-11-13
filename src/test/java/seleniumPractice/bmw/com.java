0d2b1ea909e39c43f7cba24cf768c7c2a2b105db
ernur <https://github.com/KaldybekovY/myfirstGit.git>
2022-11-13 11:41:02 -0600

    delete file

--- src/test/java/seleniumPractice/bmw/com.java
+++
@@ -1,7 +0,0 @@
-package seleniumPractice.bmw;
-
-public class com {
-    public static void main(String[] args) {
-        System.out.println("bye-bye");
-    }
-}
--- src/test/java/seleniumPractice/bmw/moc.java
+++
@@ -1,7 +0,0 @@
-package seleniumPractice.bmw;
-
-public class moc {
-    public static void main(String[] args) {
-        System.out.println("heeello all");
-    }
-}

d13bbba9c12f06818e2cccaa8c765071cde44f44
ernur <https://github.com/KaldybekovY/myfirstGit.git>
2022-12-12 17:00:39 -0600

    adding new line

--- src/test/java/seleniumPractice/NewClass.java
+++ src/test/java/seleniumPractice/NewClass.java
@@ -6,5 +6,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Heello");
         System.out.println("Bye-Bye");
+        System.out.println("Good evening");
     }
 }

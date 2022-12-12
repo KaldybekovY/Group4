52c759b872a895d20457d7b7566acd903b02956a
ernur <https://github.com/KaldybekovY/myfirstGit.git>
2022-12-12 16:45:06 -0600

    adding new file

---
+++ src/test/java/seleniumPractice/NewClass.java
@@ -0,0 +1,9 @@
+package seleniumPractice;
+
+import java.net.SocketTimeoutException;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Heello");
+    }
+}

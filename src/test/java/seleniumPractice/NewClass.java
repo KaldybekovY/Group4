1c1a3873c2d1f25e527a71f7a42875a49f95e4d5
ernur <https://github.com/KaldybekovY/myfirstGit.git>
2022-12-12 16:50:09 -0600

    adding new message

--- src/test/java/seleniumPractice/NewClass.java
+++ src/test/java/seleniumPractice/NewClass.java
@@ -5,5 +5,6 @@ import java.net.SocketTimeoutException;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Heello");
+        System.out.println("Bye-Bye");
     }
 }
